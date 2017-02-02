aac6347e29756930f2a2abeb25a162a499428fb4
Jiří Krblich <krblichjiri@gmail.com>
2017-02-02 21:59:04 +0100

    BrewInstaller [by DemonX86] SourceCode

---
+++ BrewInstaller (v1.0).sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+
+#
+#	BrewInstaller by DemonX86
+#                          ver 1.0
+#
+#
+#   LICENSE: https://opensource.org/licenses/BSD-2-Clause
+#
+
+if [ -a /usr/local/bin/brew ];then
+
+	clear
+	echo Brew is installed :)
+	exit 0
+
+else 
+
+	clear
+	/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+	echo  
+	echo  
+	echo Brew is installed :) You can try brew command
+
+fi
